3f0e789e903d109d430b91f481fdf3440dcae4e0
AztekPrideC <aztekpridec@gmail.com>
2015-10-24 15:35:50 -0500

    Se agrego la prueba del CLI y gitignore

---
+++ ugmjkill/Core/CLI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ugmjkill.Core
+{
+    public class CLI
+    {
+        public void comando(string [] args)
+        {
+            //ejemplo de algunos comandos falta agregar los demas estos solo son de pruebas
+            string comando = args[0];
+            string nombreDeLaPagina = "";
+            if (comando.ToLower().Equals("newsite"))
+            {
+                nombreDeLaPagina = args[1];
+                //el nombre sera el nombre de la carpeta
+                //crea los archivos necesarios
+            }
+            else if (comando.ToLower().Equals("help"))
+            {
+                Console.WriteLine("newsite + nombre de la pagina que se va crear");
+                Console.WriteLine("openserver / abre un servidor estatico");
+                Console.WriteLine("config -user/ el nombre de usuario");
+                Console.WriteLine("config -password/ contraseña del usuario");
+            }
+            else
+            {
+                Console.WriteLine("\"" + comando + "\" no se reoconoce como un comando ejecutable de ugmjkill ");
+            }
+            
+        }
+    }
+}
