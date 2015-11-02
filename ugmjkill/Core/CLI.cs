83decea9bb0713c44394ee0aafa5261baad8d6f5
AztekPrideC <aztekpridec@gmail.com>
2015-11-01 21:21:28 -0600

    Se modifico el CLI, se cambio if por switch con mas opciones

--- ugmjkill/Core/CLI.cs
+++ ugmjkill/Core/CLI.cs
@@ -10,26 +10,89 @@ namespace ugmjkill.Core
     {
         public void comando(string [] args)
         {
-            //ejemplo de algunos comandos falta agregar los demas estos solo son de pruebas
+
             string comando = args[0];
             string nombreDeLaPagina = "";
-            if (comando.ToLower().Equals("newsite"))
+            switch (comando.ToLower())
             {
-                nombreDeLaPagina = args[1];
-                //el nombre sera el nombre de la carpeta
-                //crea los archivos necesarios
+                case "-help":
+                case "help":
+                case "--help":
+                    Console.WriteLine("         [newsite] or [-newsite] ");
+                    Console.WriteLine("         [newsite] or [-newsite] + [-name] ");
+                    Console.WriteLine("         [openserver] abre un servidor estatico");
+                    Console.WriteLine("         [config] [-all]");
+                    Console.WriteLine("         [config] [-name] el nombre de la pagina");
+                    Console.WriteLine("         [config] [-user] el nombre de usuario");
+                    Console.WriteLine("         [config] [-password] contraseña del usuario");
+                    break;
+                case "config":
+                    switch (args[1])
+                    {
+                        case "-all":
+                        case "all":
+                            Console.WriteLine("Ingrese el nombre de la pagina");
+                            nombreDeLaPagina = Console.ReadLine();
+                            break;
+                        case "-name":
+                            Console.WriteLine("Ingrese el nombre de la pagina");
+                            nombreDeLaPagina = Console.ReadLine();
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+
+                case "newsite":
+                case "-newsite":
+                    switch (args[1])
+                    {
+                        case "-name":
+                            Console.WriteLine("Ingrese el nombre de la pagina");
+                            nombreDeLaPagina = Console.ReadLine();
+                            Console.WriteLine("Se ha creado con exito en la ruta predeterminada");
+
+                            break;
+
+                        default:
+                            if (args[1] == null)
+                            {
+                                Console.WriteLine("Se ha creado con exito en la ruta predeterminada");
+                            }
+                            else
+                            {
+                                Console.WriteLine("esta seguro que desea llamar asi " + "\"" + args[1] + "\"");
+                                string respuesta = Console.ReadLine();
+                                switch (respuesta.ToLower())
+                                {
+                                    case "si":
+                                    case "yes":
+                                        nombreDeLaPagina = args[1];
+                                        Console.WriteLine("Se ha creado con exito en la ruta predeterminada");
+                                        break;
+                                    case "no":
+                                    default:
+                                        Console.WriteLine("Se ha creado con exito en la ruta predeterminada con el nombre por default");
+                                        break;
+                                }
+                            }
+                            break;
+                    }
+
+                    break;
+
+                default:
+                    string argumentoCompleto = "";
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        argumentoCompleto += args[i] + " ";
+                    }
+                    Console.WriteLine("\"" + argumentoCompleto + "\" no se reoconoce como un comando ejecutable de ugmjkill ");
+                    break;
             }
-            else if (comando.ToLower().Equals("help"))
-            {
-                Console.WriteLine("newsite + nombre de la pagina que se va crear");
-                Console.WriteLine("openserver / abre un servidor estatico");
-                Console.WriteLine("config -user/ el nombre de usuario");
-                Console.WriteLine("config -password/ contraseña del usuario");
-            }
-            else
-            {
-                Console.WriteLine("\"" + comando + "\" no se reoconoce como un comando ejecutable de ugmjkill ");
-            }
+            
             
         }
     }
